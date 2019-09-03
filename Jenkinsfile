f0c230aac2523d945a91d732bf54f91276c8b4d9
mitchya1 <mitchell.anderson@colorado.edu>
2019-09-03 12:20:20 -0600

    initial commit

---
+++ Jenkinsfile
@@ -0,0 +1,46 @@
+pipeline{
+    agent any
+
+    options { buildDiscarder(logRotator(numToKeepStr: '5')) }
+
+    environment {
+        ROLLBAR_API_KEY = credentials("RespondologyRollbarReadOnlyApiKey")
+    }
+    stages{
+        stage("Remove artificat"){
+            steps{
+                echo "================= Removing Go binary ================="
+                sh "rm -f main"
+            }
+        }
+        stage("Build binary"){
+            steps{
+                script{
+                    echo "================= Building binary ================="
+                    sh "go build main.go"
+                }
+            }
+        }
+        stage("Checking Rollbar API"){
+            steps{
+                script{
+                    sh "./main ${ROLLBAR_API_KEY}"
+                }
+            }
+        }
+
+
+    post{
+        always{
+            echo "======== Pipeline complete ========"
+        }
+        success{
+            echo "======== pipeline executed successfully ========"
+
+        }
+        failure{
+            echo "======== pipeline execution failed ========"
+            slackSend (color: "danger", message: "FAILURE: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL}")
+        }
+    }
+}
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+var RollbarApi = "https://api.rollbar.com/api/1/items"
+
+type QueryString struct {
+	AccessToken string `json:"access_token"`
+}
+
+type RollbarResponse struct {
+	Err    int `json:"err"`
+	Result struct {
+		Items []RollbarResponseItems `json:"items"`
+	} `json:"result"`
+}
+
+type RollbarResponseItems struct {
+	LastOccurrenceTimestamp int64 `json:"last_occurrence_timestamp"`
+}
+
+type AlertMessage struct {
+	Channel string `json:"channel"`
+	Message string `json:"message"`
+	Color   string `json:"color"`
+}
+
+const slackAlertURL = "http://slack.imm.corp/api/v1/alert"
+
+func main() {
+
+	cliAccessToken := os.Args[1]
+
+	queryString := QueryString{}
+	queryString.AccessToken = cliAccessToken
+
+	fullURL := fmt.Sprintf("%s/?access_token=%s", RollbarApi, cliAccessToken)
+
+	httpResponse, err := http.Get(fullURL)
+
+	if err != nil {
+		fmt.Println("Error on request")
+		SlackAlert(0, true)
+		panic(err)
+	}
+
+	defer httpResponse.Body.Close()
+
+	rollbarResponse := RollbarResponse{}
+
+	rollbarResponseDecoder := json.NewDecoder(httpResponse.Body)
+
+	err = rollbarResponseDecoder.Decode(&rollbarResponse)
+
+	if err != nil {
+		fmt.Println("Error decoding Rollbar Response")
+		SlackAlert(0, true)
+	}
+
+	latestOccurrence := rollbarResponse.Result.Items[0].LastOccurrenceTimestamp
+
+	timeNow := time.Now()
+	timeEpochNow := timeNow.Unix()
+
+	lastOccurrenceTimeDiff := timeEpochNow - latestOccurrence
+
+	if lastOccurrenceTimeDiff > 3600 {
+		fmt.Println("Last error logged over an hour ago")
+		SlackAlert(lastOccurrenceTimeDiff, false)
+	}
+
+}
+
+func SlackAlert(timeDiff int64, e bool) {
+
+	timeInMinutes := timeDiff / 60
+
+	slackHttpClient := &http.Client{}
+
+	j := AlertMessage{}
+	j.Channel = "#alerts"
+
+	if e {
+		j.Message = "There was an error checking the Rollbar API. Please check Rollbar manually and add better error handling to this script"
+	} else {
+		j.Message = fmt.Sprintf("Respondology Rollbar last message was %d minutes ago", timeInMinutes)
+	}
+
+	j.Color = "danger"
+
+	jsonMessage, err := json.Marshal(j)
+
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = slackHttpClient.Post(slackAlertURL, "application/json", bytes.NewBuffer(jsonMessage))
+
+	if err != nil {
+		fmt.Println("Error in slackAlert", err)
+		panic(err)
+	}
+
+}
